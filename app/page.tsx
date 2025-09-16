39e3fbc30b5e54bc69f51e7f7286bf0d2e8d766d
cliff <ockiyacliffc@gmail.com>
2025-09-16 17:39:37 +0100

    button and theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>Hi Ockiya</div>
+    <div className="bg-background text-foreground" >
+      <div className="bg-primary text-primary-foreground" >Hi Ockiya</div>
+      <Image src="/profile.jpg" alt="Profile"  width={100} height={100} priority /> 
+      <Button >Click Me !</Button>
+      <div className="bg-warning text-warning-foreground p-4 rounded-md mt-4">
+        This is a warning message!
+      </div>
     </div>
   );
 }
