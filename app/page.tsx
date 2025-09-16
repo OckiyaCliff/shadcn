0701ed93b3f38d1163f2bd343477e65cd98dbc2a
cliff <ockiyacliffc@gmail.com>
2025-09-16 18:47:10 +0100

    accordion

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Faq } from "@/components/landing/faq";
 
 
 export default function Home() {
   return (
-    <div className="bg-background text-foreground" >
+    <div className="flex flex-col gap-4 text-balance bg-background text-foreground" >
       <div className="bg-primary text-primary-foreground" >Hi Ockiya</div>
       <Image src="/profile.jpg" alt="Profile"  width={100} height={100} priority /> 
       <Button >Click Me !</Button>
       <div className="bg-warning text-warning-foreground p-4 rounded-md mt-4">
         This is a warning message!
       </div>
+      <Faq /> 
     </div>
   );
 }
