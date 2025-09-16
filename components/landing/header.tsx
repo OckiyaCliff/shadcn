f3e82967e219a69b7e03170096a7c2046a833928
cliff <ockiyacliffc@gmail.com>
2025-09-16 18:34:51 +0100

    themes

---
+++ components/landing/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ModeToggle } from '../modetoggle'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+             theme switcher  
+        </div>
+        <div>
+            <ModeToggle />
+        </div>
+    </div>
+  )
+}
+
+export default Header
